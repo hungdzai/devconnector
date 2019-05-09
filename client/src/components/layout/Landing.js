804541affb19d57a347b8ac3c60cac1587148ac9
Hung Nguyen <hunnguyen@tripadvisor.com>
2019-05-09 15:46:36 -0400

    add client's layout, dashboard

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Landing = () => {
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />
+  }
+
   return (
     <section className="landing">
       <div className="dark-overlay">
@@ -11,8 +18,8 @@ const Landing = () => {
             other developers
           </p>
           <div className="buttons">
-            <a href="register.html" className="btn btn-primary">Sign Up</a>
-            <a href="login.html" className="btn btn-light">Login</a>
+            <Link to="/register" className="btn btn-primary">Sign Up</Link>
+            <Link to="/login" className="btn btn-light">Login</Link>
           </div>
         </div>
       </div>
@@ -20,4 +27,12 @@ const Landing = () => {
   )
 }
 
-export default Landing
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool
+}
+
+const mapStatetoProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStatetoProps)(Landing);
