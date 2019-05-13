9eb37db2737337ce64fd05e2914cc9ae1d8a41d7
Hung Nguyen <hunnguyen@tripadvisor.com>
2019-05-13 15:37:41 -0400

    remove comment in server.js

--- server.js
+++ server.js
@@ -29,8 +29,3 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
-// const users = require('./routes/api/users')
-// const profile = require('./routes/api/profile')
-// const posts = require('./routes/api/posts')
-
-
