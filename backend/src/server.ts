f287bdf30060b6d2a709e9aa828441e7b4474fa1
Hung Nguyen <hung@airfox.com>
2020-06-16 00:05:18 -0400

    fix: Edit CORS to allow x-auth-token header

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,7 @@ app.use(function (req: Request, res: Response, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token"
   )
   next()
 })
